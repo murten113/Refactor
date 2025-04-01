5026e19b260b8442da701862bcee90e61f68ed3a
Murmur76 <martijnlaureijs@gmail.com>
2025-04-01 12:16:15 +0200

    MORE platforms
    testing more how much the player should be able to climb

--- Assets/scripts/player script/GroundCheck.cs
+++ Assets/scripts/player script/GroundCheck.cs
@@ -10,7 +10,6 @@ public class GroundCheck : MonoBehaviour
     //draw a line to the feet of the player and check if the layer that the line touches is the ground layer, if so grounded becomes true
     private void Update()
     {
-
         grounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance, groundLayer);
         Debug.DrawRay(transform.position, Vector3.down * groundCheckDistance, Color.red);
     }
