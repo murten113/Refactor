214e01d5e39c12c86a2f0baf82e24e8b5802b2d8
Murmur76 <martijnlaureijs@gmail.com>
2025-04-02 13:27:31 +0200

    changed main menu controller to match the pause menu more

--- Assets/scripts/MainMenuController.cs
+++ Assets/scripts/MainMenuController.cs
@@ -4,6 +4,18 @@ using UnityEngine.UI;
 public class MainMenuController : MonoBehaviour
 {
 
+    public Button loadGameButton;
+    public Button quitGameButton;
+
+
+    //make listeners so when the button gets pressed it loads that function
+    private void Start()
+    {
+        loadGameButton.onClick.AddListener(LoadGame);
+        quitGameButton.onClick.AddListener(QuitGame);
+    }
+
+
     //when the button assigned to this function gets pressed it loads the scene called MainScene
     public void LoadGame()
     {
