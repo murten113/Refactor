9b0b3f789431308c231f2bce86a554fc4a92e187
Murmur76 <martijnlaureijs@gmail.com>
2025-04-02 14:26:58 +0200

    merged loose playerscripts

--- Assets/scripts/player script/PlayerScript.cs
+++ Assets/scripts/player script/PlayerScript.cs
@@ -8,6 +8,7 @@ using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
+    [Header("Basic Movement")]
     [SerializeField] public float mouseSens = 3f;
     [SerializeField] public float moveSpeed = 6f;
     [SerializeField] public float mass = 1f;
@@ -29,15 +30,39 @@ public class PlayerScript : MonoBehaviour
     internal Vector3 velocity;
     Vector2 look;
 
+    [Header("Jumping")]
+    public float jumpSpeed = 5f;
+    public float jumpPressBufferTime = .05f;
+
+    private bool tryingToJump;
+    private float lastJumpPressTime;
+
+
+    [Header("Sprinting")]
+    [SerializeField] private float speedMultiplier = 2f;
+    [SerializeField] private float maxStamina = 5f;
+    [SerializeField] private float sprintDrainRate = 1f;
+    [SerializeField] private float staminaRegenRate = 1f;
+    [SerializeField] private Slider staminaBar;
+
+    private float currentStamina;
+    private bool isSprinting;
+
+    private InputAction sprintAction;
+
 
 
     //get all the needed components when the script first starts
-   private void Awake()
+    private void Awake()
     {
         Controller = GetComponent<CharacterController>();
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions["Move"];
         lookAction = playerInput.actions["Look"];
+        sprintAction = playerInput.actions["Sprint"];
+        currentStamina = maxStamina;
+
+        SetStartStamina();
     }
 
     //lock the mouse so the players mouse doesnt move across the screen or go to a diffirent screen for those with more than one screen
@@ -53,6 +78,21 @@ public class PlayerScript : MonoBehaviour
         UpdateLook();
         UpdateMovement();
         UpdateGravity();
+
+        HandleStamina();
+    }
+
+    private void OnEnable()
+    {
+        OnBeforeMove += JumpCont;
+        OnBeforeMove += SprintCont;
+    }
+
+
+    private void OnDisable()
+    {
+        OnBeforeMove -= JumpCont;
+        OnBeforeMove -= SprintCont;
     }
 
 
@@ -103,6 +143,71 @@ public class PlayerScript : MonoBehaviour
         Controller.Move(velocity * Time.deltaTime);
     }
 
+    public void OnJump()
+    {
+        tryingToJump = true;
+        lastJumpPressTime = Time.time;
+    }
+
+    public void JumpCont()
+    {
+        bool wasTryingToJump = Time.time - lastJumpPressTime < jumpPressBufferTime;
+
+        bool isOrWasTryingToJump = tryingToJump || wasTryingToJump;
+
+        if (isOrWasTryingToJump && Controller.isGrounded)
+            velocity.y += jumpSpeed;
+
+        tryingToJump = false;
+    }
+
+
+    private void SetStartStamina()
+    {
+        if (staminaBar != null)
+        {
+            staminaBar.maxValue = maxStamina;
+            staminaBar.value = maxStamina;
+        }
+    }
+
+    //check if the player is sprinting, if so start depleting the stamina bar and quit sprinting if the stamina bar is empty. If the player isnt sprinting let he stamina bar regenerate
+    private void HandleStamina()
+    {
+        if (isSprinting)
+        {
+            currentStamina -= sprintDrainRate * Time.deltaTime;
+            if (currentStamina <= 0)
+            {
+                currentStamina = 0;
+                isSprinting = false;
+            }
+        }
+        else
+        {
+            currentStamina += staminaRegenRate * Time.deltaTime;
+            currentStamina = Mathf.Clamp(currentStamina, 0, maxStamina);
+        }
+
+        if (staminaBar != null)
+            staminaBar.value = currentStamina;
+    }
+
+    //apply a speed boost to the pleyer if theyre sprinting
+    public void SprintCont()
+    {
+        float sprintInput = sprintAction.ReadValue<float>();
+
+        if (sprintInput > 0 && currentStamina > 0)
+        {
+            isSprinting = true;
+            float forwardMovementFactor = Mathf.Clamp01(Vector3.Dot(transform.forward, velocity.normalized));
+            float multiplier = Mathf.Lerp(1f, speedMultiplier, forwardMovementFactor);
+            movementSpeedMultiplier *= multiplier;
+        }
+        else
+            isSprinting = false;
+    }
 
     //when the player is climbing turn the player script gravity off so it doesnt interfere, and when the player is grounded reset the climb timer and if he stopped climbing reapply gravity
     public void UpdateGravity()
